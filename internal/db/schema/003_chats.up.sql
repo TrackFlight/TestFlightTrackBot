907b9fb209fb919b79328366ae43509a854ba866
Laky64 <iraci.matteo@gmail.com>
2025-07-21 17:51:58 +0200

    Fixed "Chats" type

--- internal/db/schema/003_chats.up.sql
+++ internal/db/schema/003_chats.up.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS chats (
-    id         BIGSERIAL PRIMARY KEY,
+    id         BIGINT PRIMARY KEY,
     lang       VARCHAR(10) DEFAULT 'en'::CHARACTER VARYING NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
